bcd4cc751caa44c92c94bab4330a20a8142bdd04
tungnguyen1203 <tt.tung.nguyen3@tomosia.com>
2022-05-11 08:26:10 +0700

    Finish static pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
   def hello
-    render html: 'hello'
+    
   end
 end
--- test/models/user_test.rb
+++
@@ -1,7 +0,0 @@
-
-
-class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
